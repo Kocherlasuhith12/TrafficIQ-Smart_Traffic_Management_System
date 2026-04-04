89cf1beb406695301dfb5c8586ed4d034e973234
Kocherla Koteswara Suhith Sravan Babu <86333408+Kocherlasuhith12@users.noreply.github.com>
2026-04-04 23:52:39 +0530

    Update breadcrumb.tsx

--- src/components/ui/breadcrumb.tsx
+++ src/components/ui/breadcrumb.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils";
 
 const Breadcrumb = React.forwardRef<
   HTMLElement,
-  React.ComponentPropsWithoutRef<"nav"> & {
+  React.ComponentPropsWithoutRef<"nav"> & { 
     separator?: React.ReactNode;
   }
 >(({ ...props }, ref) => <nav ref={ref} aria-label="breadcrumb" {...props} />);
